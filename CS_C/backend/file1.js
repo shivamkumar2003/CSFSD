9806ce6e893b3d9e8d81fda69e4a0a65badd65c9
shivamkumar2003 <shivamkumar25510@gmail.com>
2025-02-07 12:22:28 +0530

    updated on 07/02/2025

--- CS_C/backend/file1.js
+++ CS_C/backend/file1.js
@@ -1,3 +1,14 @@
 const fs = require("fs");
 const data=fs.readFileSync("./data.txt","utf8");
 console.log(data)
+if(data.match("H")){
+    console.log("H is present")
+    const newdata=data.replace("H","AB ")
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+}
+
+// fs.writeFileSync("./data1.txt","Hello , this is my new work","utf-8");
+// fs.writeFileSync("./data1.txt", "ABES ", "utf-8");
+// fs.appendFileSync("./data1.txt", "Enginnering college", "utf-8");
+
+// fs.renameSync("./data2.txt","./data3.txt");
